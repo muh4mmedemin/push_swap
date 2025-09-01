0442e287cd68fc6eda3dba1e5aa158817a31c725
Muhammed Emin Ayna <muayna@k1m07s07.42istanbul.com.tr>
2025-09-01 18:20:02 +0300

    Finish Makefile

--- push_swap.c
+++ push_swap.c
@@ -6,17 +6,25 @@
 /*   By: muayna <muayna@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/26 14:54:36 by muayna            #+#    #+#             */
-/*   Updated: 2025/09/01 16:27:14 by muayna           ###   ########.fr       */
+/*   Updated: 2025/09/01 18:16:12 by muayna           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
 #include "libft/libft.h"
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
-     (void)argc;
-     int d =ft_atoi(argv[1]);
-     printf("%d", d);
+     argc--;
+     t_stack *a;
+     int d;
+     a = malloc(sizeof(t_stack));
+     while (argc >= 1)
+     {
+          d = ft_atoi(argv[argc]);
+          argc--;
+     }
+     printf("%d" ,d);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,11 +6,16 @@
 /*   By: muayna <muayna@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/01 16:17:07 by muayna            #+#    #+#             */
-/*   Updated: 2025/09/01 16:21:20 by muayna           ###   ########.fr       */
+/*   Updated: 2025/09/01 17:50:09 by muayna           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
-void sa(int argc, char *argv[]);
+     void sa(int argc, char *argv[]);
+     typedef struct stack
+     {
+	     int			content;
+	     struct stack	*next;
+     }    t_stack;
 #endif
