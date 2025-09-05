5709b84662fe1fd8e8c43fbf6146667ac0cec855
Muhammed Emin Ayna <muayna@k1m13s05.42istanbul.com.tr>
2025-09-05 20:03:29 +0300

    Example 2

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: muayna <muayna@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/26 14:54:36 by muayna            #+#    #+#             */
-/*   Updated: 2025/09/05 15:57:45 by muayna           ###   ########.fr       */
+/*   Updated: 2025/09/05 18:00:19 by muayna           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,42 @@
 #include "libft/libft.h"
 #include <stdlib.h>
 
+void ft_fill_stack(t_stack **a, int argc, char **argv)
+{
+     int i = 1;
+     t_stack *temp;
+     temp = *a;
+     while(argc > 0)
+     {
+          temp->content=ft_atoi(argv[i]);
+          if(argc != 0)
+          {
+               temp->next=malloc(sizeof(t_stack));
+          }
+          temp = temp->next;
+          i++;
+          argc--;
+     }
+}
+
 int main(int argc, char *argv[])
 {
      argc--;
      int i = 1;
      t_stack *a;
-     t_stack *temp;
-     a = malloc(sizeof(t_stack));
-     temp = a;
-    while(argc > 0)
+     t_stack *b;
+     b = a;
+     if(argc != 0)
      {
-          temp->content=ft_atoi(argv[i]);
-          temp->next=malloc(sizeof(t_stack));
-          temp = temp->next;
-          i++;
-          argc--;
+          a = malloc(sizeof(t_stack));
+          ft_fill_stack(&a, argc, argv);
      }
      while(a->next != NULL)
      {
-          printf("%d", (int)a->content);
+          printf("%d\n", (int)a->content);
           a = a->next;
      }
+     free(b);
      /*a = malloc(sizeof(t_stack));
      a->content=ft_atoi(argv[1]);
      a->next=malloc(sizeof(t_stack));
