5012a81a0c13ceb84a1fdcaa134ad29c9d4cc589
Muhammed Emin Ayna <muayna@k1m03s06.42istanbul.com.tr>
2025-09-10 19:32:04 +0300

    Tets

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: muayna <muayna@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/26 14:54:36 by muayna            #+#    #+#             */
-/*   Updated: 2025/09/10 17:31:42 by muayna           ###   ########.fr       */
+/*   Updated: 2025/09/10 19:31:24 by muayna           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,45 @@
 #include <stdio.h>
 #include "libft/libft.h"
 #include <stdlib.h>
+
+void find_index(t_stack *stack, int argc)
+{
+     t_stack *tmp;
+     t_stack *small;
+     int index = 0;
+     int s1;
+     int tmpargc = argc;
+     tmp = stack;
+     while (argc > 0)
+     {
+          tmp = stack;
+          s1 = tmp->content;
+          while (tmp != NULL)
+          {
+               tmp = tmp->next;
+               if (tmp != NULL && s1 > tmp->content)
+               {
+                    s1 = tmp->content;
+                    small = tmp;
+               }
+          }
+          stack = stack->next;
+          argc--;
+     }
+     printf("%d", s1);
+}
+
+int lst_size(t_stack *stack)
+{
+     int size;
+     size = 0;
+     while (stack->next != NULL)
+     {
+          stack = stack->next;
+          size++;
+     }
+     return size;
+}
 
 void free_stack(t_stack *stack)
 {
@@ -47,19 +86,21 @@ void ft_fill_stack(t_stack **a, int argc, char **argv)
 int main(int argc, char *argv[])
 {
      argc--;
-     int i = 1;
+     int size = 0;
      t_stack *a;
-     t_stack *b;
+     t_stack *tmp;
      if(argc != 0)
      {
           a = malloc(sizeof(t_stack));
           ft_fill_stack(&a, argc, argv);
+          size = lst_size(a);
      }
-     b = a;
-     while(a->next != NULL)
+     find_index(a, argc);
+     tmp = a;
+     /*while(a->next != NULL)
      {
           printf("%d\n", (int)a->content);
           a = a->next;
-     }
-     free_stack(b);
+     }*/
+     free_stack(tmp);
 }
